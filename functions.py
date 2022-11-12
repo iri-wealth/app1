6b17dbedda971f9615c5e616b10e0de31e46e8e5
Irina Kainz <irina_kainz@yahoo.com>
2022-11-12 12:54:19 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+def get_todos(filepath="todos.txt"):
+    """
+    defines parameter which reads the text
+    file and returns the list of
+    todo item :param filepath: :return:
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+print(__name__)
+if __name__=="__main__":
+    print("hello from functions")
+    print(get_todos())
+
