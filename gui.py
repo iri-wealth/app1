481a8d2a0b671a4f2df0991c88c69eccb4ef8aa7
Irina Kainz <irina_kainz@yahoo.com>
2022-11-13 13:46:44 +0100

    Create basic GUI convertor as a new coding exercise, Day 16

--- gui.py
+++ gui.py
@@ -4,9 +4,11 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 print("Thank you for your input")
 window.close()
+
 
