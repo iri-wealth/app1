65f6768412f42a76e97b480cffd312301520c2b8
Irina Kainz <irina_kainz@yahoo.com>
2022-11-13 13:20:55 +0100

    Create basic GUI without interractions, Day 16

--- cli.py
+++ cli.py
@@ -1,23 +1,24 @@
-from functions import get_todos, write_todos
+import functions
 import time
+
 now = time.strftime("%b %d, %Y %H:%M:%S")
-print("The time is below:")
 print("It is", now)
 
 while True:
     user_action = input("Type Add, Show, Edit, Complete or Exit: ")
     user_action = user_action.strip()
+
     if user_action.startswith("Add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("Show"):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -31,12 +32,12 @@ while True:
 
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid")#
@@ -45,12 +46,12 @@ while True:
     elif user_action.startswith("Complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt" # "todos_item.txt"
+
+def get_todos(filepath=FILEPATH): # '"todos.txt"):
     """
     defines parameter which reads the text
     file and returns the list of
@@ -8,10 +10,10 @@ def get_todos(filepath="todos.txt"):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH): # "todos.txt"):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
-print(__name__)
+
 if __name__=="__main__":
     print("hello from functions")
     print(get_todos())
