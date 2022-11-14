c6d65c034b23e570caaf66261e21e62fdd018e60
Irina Kainz <irina_kainz@yahoo.com>
2022-11-14 18:17:54 +0100

    Implemented GUI, Window, labels, inputs and one button

--- gui.py
+++ gui.py
@@ -8,8 +8,10 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
+layout = [[label], [input_box, add_button], [list_box, edit_button]]
+
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -38,7 +40,7 @@ while True:
         case sg.WIN_CLOSED:
             break
 
-
+print("Thank you.Bye")
 window.close()
 
 
